f94405e9a6805115521bae46dd75f26c6971fcf9
catalingavan <catalingavan@gmail.com>
2020-02-27 08:19:54 +0200

    in progress

--- source/_static/js/docs.js
+++ source/_static/js/docs.js
@@ -4,8 +4,17 @@ function init() {
     $tocMenu.find("ul").each(function() {
         var $parentLi = $(this).parent("li");
         if($parentLi.length) {
-            $parentLi.children(".reference").prepend("<span class='toctree-expand'></span>");
+            $parentLi.children("a").prepend("<span class='toctree-expand'></span>");
         }
+    });
+
+    $tocMenu.find("li.current").addClass("expanded");
+
+    $tocMenu.on("click", ".toctree-expand", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        $(this).closest("li").toggleClass("expanded");
     });
 };
 
